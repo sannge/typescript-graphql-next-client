0aa67505f3a6dfd22d29dbb7c52a94cc35a9fcb0
San Nge <samngestep@gmail.com>
2021-02-13 21:33:30 -0800

    finished login and register and navbar

---
+++ src/pages/_app.tsx
@@ -0,0 +1,90 @@
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+
+import theme from "../theme";
+import {
+	LoginMutation,
+	MeDocument,
+	MeQuery,
+	RegisterMutation,
+} from "../generated/graphql";
+
+function betterUpdateQuery<Result, Query>(
+	cache: Cache,
+	qi: QueryInput,
+	result: any,
+	fn: (r: Result, q: Query) => Query
+) {
+	return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
+
+const client = createClient({
+	url: "http://localhost:4001/graphql",
+	fetchOptions: {
+		credentials: "include",
+	},
+	exchanges: [
+		dedupExchange,
+		cacheExchange({
+			updates: {
+				Mutation: {
+					login: (_result, _, cache) => {
+						betterUpdateQuery<LoginMutation, MeQuery>(
+							cache,
+							{
+								query: MeDocument,
+							},
+							_result,
+							(result, query) => {
+								if (result.login.errors) {
+									return query;
+								} else {
+									return {
+										me: result.login.user,
+									};
+								}
+							}
+						);
+					},
+					register: (_result, _, cache) => {
+						betterUpdateQuery<RegisterMutation, MeQuery>(
+							cache,
+							{
+								query: MeDocument,
+							},
+							_result,
+							(result, query) => {
+								if (result.register.errors) {
+									return query;
+								} else {
+									return {
+										me: result.register.user,
+									};
+								}
+							}
+						);
+					},
+				},
+			},
+		}),
+		fetchExchange,
+	],
+});
+
+function MyApp({ Component, pageProps }: any) {
+	return (
+		<Provider value={client}>
+			<ChakraProvider resetCSS theme={theme}>
+				<ColorModeProvider
+					options={{
+						useSystemColorMode: true,
+					}}>
+					<Component {...pageProps} />
+				</ColorModeProvider>
+			</ChakraProvider>
+		</Provider>
+	);
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar";
+
+const Index = () => {
+	return (
+		<>
+			<NavBar />
+			<div>Hello World</div>
+		</>
+	);
+};
+
+export default Index;
